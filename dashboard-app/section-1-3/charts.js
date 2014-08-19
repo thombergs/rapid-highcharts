54b745b6ada34b24debdc8eb479d2adfa6022b1b
Tom Hombergs <tom.hombergs@gmail.com>
2014-08-19 08:30:09 +0200

    added section 2-1

--- dashboard-app/section-1-3/charts.js
+++ dashboard-app/section-1-3/charts.js
@@ -1,21 +1,5 @@
 $(function () {
 
-    $(function () {
-        $('#container').highcharts({
-            title: {
-                text: 'Page Hits with different browsers'
-            },
-            series: [{
-                type: 'pie',
-                name: 'Page Hits',
-                data: [
-                    ['IE',       98],
-                    ['Others',   56],
-                    ['Chrome', 42]
-                ]
-            }]
-        });
-    });
 
 });
 
---
+++ dashboard-app/section-2-1/charts.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $('#browserChart').highcharts({
+        title: {
+            text: 'Page Hits with different browsers'
+        },
+        series: [{
+            type: 'pie',
+            name: 'Page Hits',
+            data: [
+                ['IE',       98],
+                ['Others',   56],
+                ['Chrome', 42]
+            ]
+        }]
+    });
+
+});
+
+
+
+
