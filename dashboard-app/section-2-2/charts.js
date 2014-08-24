3ed82254513cf1df2305a9f16ac5e8bb4a144fac
Tom Hombergs <tom.hombergs@gmail.com>
2014-08-24 12:06:51 +0200

    finished section 2-3

--- dashboard-app/section-2-2/charts.js
+++ dashboard-app/section-2-2/charts.js
@@ -34,7 +34,7 @@ $(function () {
         series: [{
             pointInterval: 24*60*60*1000,
             pointStart: Date.now() - 29*24*60*60*1000,
-            name: 'Tokyo',
+            name: 'Page Hits',
             data: [5,9,6,20,23,15,8,7,35,30,
                 40,34,27,23,13,50,44,34,36,
                 30,28,26,20,37,43,52,55,60,66,55]
