f752df325350ba7ddb1b68bbd9b3e7bdfb0e9b19
Tom Hombergs <tom.hombergs@gmail.com>
2014-08-26 08:06:08 +0200

    section 2-4

--- dashboard-app/section-2-5/charts.js
+++ dashboard-app/section-2-5/charts.js
@@ -313,69 +313,53 @@ $(function () {
     });
 
 
-    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=world-population-density.json&callback=?', function (data) {
+    // Initiate the chart
+    $('#mapChart').highcharts('Map', {
 
-        // Add lower case codes to the data set for inclusion in the tooltip.pointFormat
-        $.each(data, function () {
-            this.flag = this.code.replace('UK', 'GB').toLowerCase();
-        });
-
-        // Initiate the chart
-        $('#mapChart').highcharts('Map', {
-
+        legend: {
             title: {
-                text: 'Fixed tooltip with HTML'
-            },
-
-            legend: {
-                title: {
-                    text: 'Population density per km²',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
-                    }
+                text: 'Page Hits',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'black'
                 }
-            },
+            }
+        },
 
-            mapNavigation: {
-                enabled: true,
-                buttonOptions: {
-                    verticalAlign: 'bottom'
+        mapNavigation: {
+            enabled: true,
+            buttonOptions: {
+                verticalAlign: 'bottom'
+            }
+        },
+
+        tooltip: {
+            backgroundColor: 'none',
+            borderWidth: 0,
+            shadow: false,
+            useHTML: true,
+            padding: 0,
+            pointFormat: '{point.name}: <b>{point.value}</b>',
+            positioner: function () {
+                return { x: 0, y: 250 };
+            }
+        },
+
+        colorAxis: {
+            min: 1,
+            max: 1000,
+        },
+
+        series : [{
+            data : mapData,
+            mapData: Highcharts.maps['custom/world'],
+            joinBy: ['iso-a2', 'code'],
+            name: 'Page Hits',
+            states: {
+                hover: {
+                    color: '#BADA55'
                 }
-            },
-
-            tooltip: {
-                backgroundColor: 'none',
-                borderWidth: 0,
-                shadow: false,
-                useHTML: true,
-                padding: 0,
-                pointFormat: '<span class="f32"><span class="flag {point.flag}"></span></span>'
-                    + ' {point.name}: <b>{point.value}</b>/km²',
-                positioner: function () {
-                    return { x: 0, y: 250 };
-                }
-            },
-
-            colorAxis: {
-                min: 1,
-                max: 1000,
-                type: 'logarithmic'
-            },
-
-            series: [
-                {
-                    data: data,
-                    mapData: Highcharts.maps['custom/world'],
-                    joinBy: ['iso-a2', 'code'],
-                    name: 'Population density',
-                    states: {
-                        hover: {
-                            color: '#BADA55'
-                        }
-                    }
-                }
-            ]
-        });
+            }
+        }]
     });
 
 });
